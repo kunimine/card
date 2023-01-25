4afc3146a8b15f93e31cffdefa01b6304ec331b2
henaer <henaer@tom.com>
2023-01-25 20:39:09 +0800

    20230125

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import main from "./components/main.vue";
 </script>
 
 <template>
@@ -13,7 +12,7 @@ import TheWelcome from "./components/TheWelcome.vue";
     <img
       alt="H.N.E-KU logo"
       class="logo"
-      src="./assets/Sprite-0001.svg"
+      src="./assets/HLogo2.svg"
       width="125"
       height="125"
     />
@@ -22,10 +21,6 @@ import TheWelcome from "./components/TheWelcome.vue";
       <HelloWorld msg="You did it!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
@@ -36,23 +31,5 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
---
+++ src/components/floot.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>&copy;2021-2022 河南赫纳尔科技有限公司</p>
+  <p>&copy;2021 HeNaerx提供服务</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
